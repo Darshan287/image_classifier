9018cfee9f9cbc3263d6ba9ff333ad61ef8aef0b
Darshan <153099496+Darshan287@users.noreply.github.com>
2025-09-13 10:31:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
 (
     MobileNetV2,
